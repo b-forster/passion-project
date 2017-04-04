70388f60f942b81aa6b552724c11878ce6356fd0
Brianna Forster <brianna.forster@gmail.com>
2017-04-03 21:47:13 -0700

    Add bcrypt methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
 end
